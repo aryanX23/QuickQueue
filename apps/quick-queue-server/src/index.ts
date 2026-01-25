09a4ead746dea4d0b6284f1e4cd271c0733f2767
aryanX23 <aryan@transfi.com>
2026-01-25 16:36:25 +0530

    feat: enhance Quick Queue Server with improved error handling and graceful shutdown procedures

--- apps/quick-queue-server/src/index.ts
+++ apps/quick-queue-server/src/index.ts
@@ -3,12 +3,44 @@ dotenv.config();
 
 import { setupExpress } from '@/configurations/express-configurations';
 
-const startServer = async () => {
+const startServer = () => {
   const app = setupExpress();
   const port = process.env.PORT || 8000;
-  app.listen(port, () => {
+  const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
+  server.on('error', (error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+  process.on('SIGINT', () => {
+    console.log('Server is shutting down...');
+    server.close(() => {
+      console.log('Server has been shut down');
+      process.exit(0);
+    });
+  });
+
+  process.on('SIGTERM', () => {
+    console.log('Server is shutting down...');
+    server.close(() => {
+      console.log('Server has been shut down');
+      process.exit(0);
+    });
+  });
+
+  process.on('uncaughtException', (error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+  process.on('unhandledRejection', (error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+  return server;
 };
 
 startServer();
